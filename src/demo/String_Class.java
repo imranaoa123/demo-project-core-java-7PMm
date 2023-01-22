1ff77e5e395b85f8d4a104e5f36a16e2689e6352
corejava <imranaoa@gmail.com>
2023-01-22 04:18:00 -0800

    String Classes

---
+++ src/demo/String_Class.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class String_Class {
+    public static void main(String[] args) {
+        String name = "Imran Khan";
+        System.out.println("My name is :"+name);
+    }
+}
