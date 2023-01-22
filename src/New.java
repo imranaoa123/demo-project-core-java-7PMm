3cb066e18de260c0edd9d72eb78cd8276560d794
corejava <imranaoa@gmail.com>
2023-01-22 04:11:36 -0800

    The new project

--- src/New.java
+++ src/New.java
@@ -1,5 +1,9 @@
 public class New {
     public static void main(String[] args) {
         System.out.println("My first class");
-        System.out.println("My first class");  }
+        System.out.println("My first class");
+        System.out.println("My first class");
+        System.out.println("My first class");
+    }
+
 }
