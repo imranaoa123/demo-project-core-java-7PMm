87a06c448b863f8ab234a7b9599f9a909912c012
corejava <imranaoa@gmail.com>
2023-01-25 03:39:29 -0800

    String Classes

---
+++ src/variablesprint/Variablesimran.java
@@ -0,0 +1,35 @@
+package variablesprint;
+
+public class Variablesimran {
+    public static void main(String[] args) {
+        String cityname;
+        cityname="Tundla";
+        System.out.println(cityname);
+        System.out.println("my city name is:"+cityname);
+
+
+        int age;
+        age=35;
+        System.out.println(age);
+        System.out.println("my age is"+age);
+
+
+        double price;
+        price=355.54;
+        System.out.println(price);
+
+
+        String industry;
+        industry="Tcs";
+        System.out.println("industry "+industry);
+
+        String car;
+        car="Audy";
+        System.out.println(car);
+
+
+
+
+
+    }
+}
